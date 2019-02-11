9547f8718ec994d40f0a1588afda7f4ef5c3ab92
iamshadmirza <md.shad1@yahoo.com>
2019-02-11 10:02:44 +0530

    api key hidden now

--- App.js
+++ App.js
@@ -4,20 +4,14 @@ import Router from './src/component/Router';
 import { Provider } from 'react-redux';
 import { Font } from 'expo';
 import store from './src/redux/store';
+import config from './src/networking/firebase-config';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
   }
   componentWillMount() {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyAeGRczbZOYARmRV0PAFpgl9rMh2UDIqUg',
-      authDomain: 'taskmaker-70192.firebaseapp.com',
-      databaseURL: 'https://taskmaker-70192.firebaseio.com',
-      projectId: 'taskmaker-70192',
-      storageBucket: 'taskmaker-70192.appspot.com',
-      messagingSenderId: '200199286623'
-    });
+    firebase.initializeApp(config);
   }
   componentDidMount() {
     Font.loadAsync({
