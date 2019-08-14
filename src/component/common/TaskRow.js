c499681dbb4ae43cd81c5cded031b38a45391e1a
Shad <iamshadmirza@gmail.com>
2019-08-14 06:37:19 +0530

    PanGestureHandler added

--- src/component/common/TaskRow.js
+++ src/component/common/TaskRow.js
@@ -1,21 +1,10 @@
-import React, { useRef } from 'react';
-import { Text, TouchableOpacity, StyleSheet, Animated, PanResponder } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import colors from 'res/colors.json';
 
 const TaskRow = (props) => {
-    const _panResponder = useRef(PanResponder.create({
-        onStartShouldSetPanResponder: () => true,
-        onMoveShouldSetPanResponder: () => true,
-        onPanResponderRelease: (event, { dx }) => {
-            if (dx > 120) {
-                markDone();
-            }
-            if (dx < 120) {
-                unMarkDone();
-            }
-        }
-    }));
 
     markDone = () => {
         props.onMarkDone(props.task);
@@ -27,23 +16,36 @@ const TaskRow = (props) => {
         props.onDelete(props.task);
     }
 
+    onHandlerStateChange = ({ nativeEvent }) => {
+        if (nativeEvent.state === State.END) {
+            if (nativeEvent.translationX > 100) {
+                markDone();
+            }
+            if (nativeEvent.translationX < -100) {
+                unMarkDone();
+            }
+        }
+    }
+
     return (
-        <Animated.View style={styles.container} {..._panResponder.current.panHandlers}>
-            {props.done ?
-                <TouchableOpacity onPress={unMarkDone}>
-                    <Ionicons name='ios-checkmark-circle' size={20} color={colors.grey} />
-                </TouchableOpacity> :
-                <TouchableOpacity onPress={markDone}>
-                    <Ionicons name='ios-radio-button-off' size={20} color={colors.black} />
-                </TouchableOpacity>}
-            <Text style={[styles.rowText, props.done ? { textDecorationLine: 'line-through', color: colors.grey } : null]}>
-                {props.children}
-            </Text>
-            {props.done ?
-                <TouchableOpacity onPress={deleteTask}>
-                    <Ionicons name='ios-close-circle' size={20} color={colors.grey} />
-                </TouchableOpacity> : null}
-        </Animated.View>
+        <PanGestureHandler onHandlerStateChange={onHandlerStateChange}>
+            <View style={styles.container}>
+                {props.done ?
+                    <TouchableOpacity onPress={unMarkDone}>
+                        <Ionicons name='ios-checkmark-circle' size={20} color={colors.grey} />
+                    </TouchableOpacity> :
+                    <TouchableOpacity onPress={markDone}>
+                        <Ionicons name='ios-radio-button-off' size={20} color={colors.black} />
+                    </TouchableOpacity>}
+                <Text style={[styles.rowText, props.done ? { textDecorationLine: 'line-through', color: colors.grey } : null]}>
+                    {props.children}
+                </Text>
+                {props.done ?
+                    <TouchableOpacity onPress={deleteTask}>
+                        <Ionicons name='ios-close-circle' size={20} color={colors.grey} />
+                    </TouchableOpacity> : null}
+            </View>
+        </PanGestureHandler>
     );
 }
 
@@ -52,7 +54,6 @@ const styles = StyleSheet.create({
         elevation: 1,
         margin: 5,
         padding: 5,
-        backgroundColor: 'red',
         flexDirection: 'row',
         justifyContent: 'flex-start'
     },
